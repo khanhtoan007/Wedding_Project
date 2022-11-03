8fc243078a96cd7dc900a9e14539478206a1bbbb
Quoc <quoctmde160598@fpt.edu.vn>
2022-11-03 18:39:15 +0700

    uh

--- src/main/java/context/DBContext.java
+++ src/main/java/context/DBContext.java
@@ -17,7 +17,7 @@ public class DBContext {
 //    }
     /*Insert your other code right after this comment*/
     /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
-    private final String serverName = "LAPTOP-99CNG1A4";
+    private final String serverName = "QUOC\\SQLEXPRESS";
     private final String dbName = "Wedding_Project";
     private final String portNumber = "1433";
     private final String instance="";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
